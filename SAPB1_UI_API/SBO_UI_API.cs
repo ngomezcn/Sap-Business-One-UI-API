71561e47151619eb927c077d9a120264138f4bde
Naim Gomez <naimgomezcn@gmail.com>
2022-09-06 11:26:41 +0200

    creacion de formulario y guardado en XML

--- SAPB1_UI_API/SBO_UI_API.cs
+++ SAPB1_UI_API/SBO_UI_API.cs
@@ -10,6 +10,7 @@ namespace SAPB1_UI_API
     internal class SBO_UI_API
     {
         private SAPbouiCOM.Application SBO_Application;
+        private SAPbouiCOM.Form oForm;
 
         public void ConnectToSBO()
         {
@@ -23,8 +24,6 @@ namespace SAPB1_UI_API
                 SBO_Application = sboGuiApi.GetApplication(-1);
 
                 SBO_Application.StatusBar.SetText("Add-on cargado correctamente", SAPbouiCOM.BoMessageTime.bmt_Medium, SAPbouiCOM.BoStatusBarMessageType.smt_Success);
-                SBO_Application.MessageBox("Add-on cargado correctametne");
-
             }
             catch (Exception ex)
             {
@@ -35,7 +34,11 @@ namespace SAPB1_UI_API
         public SBO_UI_API()
         {
             ConnectToSBO();
-            bindEventHandlersToSBO();
+            //bindEventHandlersToSBO();
+
+            CrearFormularioSimple();
+            oForm.Visible = true;
+            GuardarComoXML();
         }
 
         private void bindEventHandlersToSBO()
@@ -43,7 +46,7 @@ namespace SAPB1_UI_API
             // Eventos gestionados por SBO_Application, se debe pasar el evento tal como se especifica en la documentación
             // Para ver todos los tipos de eventos que gestiona SBO ver la interfaz: SAPbouiCOM._IApplicationEvents_Event 
 
-            SBO_Application.AppEvent += new SAPbouiCOM._IApplicationEvents_AppEventEventHandler(SBO_Application_AppEvent);
+            SBO_Application.AppEvent  += new SAPbouiCOM._IApplicationEvents_AppEventEventHandler(SBO_Application_AppEvent);
             SBO_Application.MenuEvent += new SAPbouiCOM._IApplicationEvents_MenuEventEventHandler(SBO_Application_MenuEvent);
             SBO_Application.ItemEvent += new SAPbouiCOM._IApplicationEvents_ItemEventEventHandler(SBO_Application_ItemEvent);
         }
@@ -126,8 +129,138 @@ namespace SAPB1_UI_API
                 // ...
                 // ...
 
-                Console.Write("An " + EventEnum.ToString() + " has been sent by a form with the unique ID: " + FormUID + "\n");
+                Console.Write("Un evento tipo " + EventEnum.ToString() + " ha sido enviado desde un formulario con ID: " + FormUID + "\n");
             }
+        }
+
+        private void CrearFormularioSimple()
+        {
+            SAPbouiCOM.Item oItem = null;
+
+            SAPbouiCOM.Button oButton = null;
+            SAPbouiCOM.StaticText oStaticText = null;
+            SAPbouiCOM.EditText oEditText = null;
+            SAPbouiCOM.ComboBox oComboBox = null;
+
+            SAPbouiCOM.FormCreationParams oCreationParams = null;
+
+            // Creando parametros del formulario
+            oCreationParams = SBO_Application.CreateObject(SAPbouiCOM.BoCreatableObjectType.cot_FormCreationParams);
+            oCreationParams.BorderStyle = SAPbouiCOM.BoFormBorderStyle.fbs_Fixed;
+            oCreationParams.UniqueID = "dfga";
+
+            // Creando formulario a partir de los parametros indicados
+            
+            oForm = SBO_Application.Forms.AddEx(oCreationParams);
+
+            // Creando los datos de entrada (esto todavia no muestra nada hay que bindearlo a un item)
+            oForm.DataSources.UserDataSources.Add("EditSource", SAPbouiCOM.BoDataType.dt_SHORT_TEXT, 20);
+            oForm.DataSources.UserDataSources.Add("CombSource", SAPbouiCOM.BoDataType.dt_SHORT_TEXT, 20);
+
+            // Algunas propiedades del nuevo formulario
+            oForm.Title = "Simple Form";
+            oForm.Left = 400;
+            oForm.Top = 100;
+            oForm.ClientHeight = 80;
+            oForm.ClientWidth = 350;
+
+            // SBO gestionara automaticamente los eventos si ponemos el UID a 1
+            // ** Añadimos boton de OK **
+            oItem = oForm.Items.Add("1", SAPbouiCOM.BoFormItemTypes.it_BUTTON);
+            oItem.Left = 6;
+            oItem.Width = 65;
+            oItem.Top = 51;
+            oItem.Height = 19;
+            oButton = ((SAPbouiCOM.Button)(oItem.Specific));
+            oButton.Caption = "Ok";
+
+            // SBO gestionara automaticamente los eventos si ponemos el UID a 2
+            // ** Añadimos boton de Cancelar ** 
+            oItem = oForm.Items.Add("2", SAPbouiCOM.BoFormItemTypes.it_BUTTON);
+            oItem.Left = 75;
+            oItem.Width = 65;
+            oItem.Top = 51;
+            oItem.Height = 19;
+            oButton = ((SAPbouiCOM.Button)(oItem.Specific));
+            oButton.Caption = "Cancel";
+
+            // ** Añadimos un rectangulo **
+            oItem = oForm.Items.Add("Rect1", SAPbouiCOM.BoFormItemTypes.it_RECTANGLE);
+            oItem.Left = 0;
+            oItem.Width = 344;
+            oItem.Top = 1;
+            oItem.Height = 49;
+
+            // ** Añadimos Texto Estatico **
+            oItem = oForm.Items.Add("StaticTxt1", SAPbouiCOM.BoFormItemTypes.it_STATIC);
+            oItem.Left = 7;
+            oItem.Width = 148;
+            oItem.Top = 8;
+            oItem.Height = 14;
+            oItem.LinkTo = "EditText1";
+
+            oStaticText = ((SAPbouiCOM.StaticText)(oItem.Specific));
+            oStaticText.Caption = "Static Text 1";
+
+            // ** Añadimos otro Texto Estatico **
+            oItem = oForm.Items.Add("StaticTxt2", SAPbouiCOM.BoFormItemTypes.it_STATIC);
+            oItem.Left = 7;
+            oItem.Width = 148;
+            oItem.Top = 24;
+            oItem.Height = 14;
+            oItem.LinkTo = "ComboBox1";
+
+            oStaticText = ((SAPbouiCOM.StaticText)(oItem.Specific));
+            oStaticText.Caption = "Static Text 2";
+
+            // ** Añadimos Edit Text **
+            oItem = oForm.Items.Add("EditText1", SAPbouiCOM.BoFormItemTypes.it_EDIT);
+            oItem.Left = 157;
+            oItem.Width = 163;
+            oItem.Top = 8;
+            oItem.Height = 14;
+
+            oEditText = ((SAPbouiCOM.EditText)(oItem.Specific));
+            oEditText.DataBind.SetBound(true, "", "EditSource"); // Bindeamos el Edit Text a los datos de entrada creados anteriormente 
+
+            oEditText.String = "Edit Text 1";
+
+            // ** Añadimos Combo Box **
+            oItem = oForm.Items.Add("ComboBox1", SAPbouiCOM.BoFormItemTypes.it_COMBO_BOX);
+            oItem.Left = 157;
+            oItem.Width = 163;
+            oItem.Top = 24;
+            oItem.Height = 14;
+            oItem.DisplayDesc = false;
+
+            oComboBox = ((SAPbouiCOM.ComboBox)(oItem.Specific));
+            oComboBox.DataBind.SetBound(true, "", "CombSource"); // Bindeamos el Edit Text a los datos de entrada creados anteriormente 
+
+            oComboBox.ValidValues.Add("1", "Combo Value 1");
+            oComboBox.ValidValues.Add("2", "Combo Value 2");
+            oComboBox.ValidValues.Add("3", "Combo Value 3");
+        }
+
+        private void GuardarComoXML()
+        {
+            System.Xml.XmlDocument oXmlDoc = null;
+
+            oXmlDoc = new System.Xml.XmlDocument();
+
+            string sXmlString = null;
+
+            // get the form as an XML string
+            sXmlString = oForm.GetAsXML();
+
+            // load the form's XML string to the
+            // XML document object
+            oXmlDoc.LoadXml(sXmlString);
+
+            string sPath = null;
+
+            sPath = System.IO.Directory.GetParent(Application.StartupPath).ToString();
+
+            oXmlDoc.Save((sPath + @"\MySimpleForm.xml"));
         }
     }
 }
